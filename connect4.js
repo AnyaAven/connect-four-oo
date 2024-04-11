a3437139f47828f0f1b1265d1b49d526bcf53ac6
Anya Aven <leafeon96@gmail.com>
2024-04-11 15:12:04 -0400

    converted connect4's logic and data to a Class

--- connect4.js
+++ connect4.js
@@ -5,63 +5,83 @@
  * board fills (tie).
  */
 
-const WIDTH = 7;
-const HEIGHT = 6;
+class Game {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
 
-const gameState = {
-  currPlayer: 1, // active player: 1 or 2
-  board: Array(HEIGHT), // array of HEIGHT number of slots
-  // Each array slot is empty to start, but will be filled in with an array
-  // of WIDTH later.
-  // These inner arrays will represent rows.
-  // gameState.board[HEIGHT][0] represents the bottom-left spot on the board
-};
+    //TODO: do we need this?
+    this.currPlayer = 1;
 
-
-/** switchCurrPlayer:
- *   checks the value of currPlayer and swaps the value to the other
- *   player instance
- */
-
-function switchCurrPlayer() {
-  gameState.currPlayer = gameState.currPlayer === 1 ? 2 : 1;
-}
-
-
-/** makeBoard: fill in global `board`:
- *    board = array of rows, each row is array of cells  (board[y][x])
-*/
-
-function makeBoard() {
-  for (let y = 0; y < HEIGHT; y++) {
-    const emptyRow = Array(WIDTH).fill(null);
-    gameState.board[y] = emptyRow;
+    //TODO: does this work as we intended?
+    this.board = this.makeBoard();
   }
 
-  // alternatively:
-  // gameState.board = [...gameState.board].map(() => Array(WIDTH).fill(null));
-}
+  /** switchCurrPlayer:
+   *   checks the value of currPlayer and swaps the value to the other
+   *   player instance
+   */
+  switchCurrPlayer() {
+    this.currPlayer = this.currPlayer === 1 ? 2 : 1;
+  }
 
 
-/** findSpotInCol: given column x, return y coordinate of furthest-down spot
- *    (return null if filled)
- */
+  /** makeBoard: fill in global `board`:
+   *    board = array of rows, each row is array of cells  (board[y][x])
+  */
 
-function findSpotInCol(x) {
-  for (let y = HEIGHT - 1; y >= 0; y--) {
-    if (gameState.board[y][x] === null) {
-      return y;
+  makeBoard() {
+    const board = [];
+
+    for (let y = 0; y < this.height; y++) {
+      const emptyRow = Array(this.width).fill(null);
+      board[y] = emptyRow;
     }
+
+    return board;
   }
-  return null;
-}
 
 
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
+  /** findSpotInCol: given column x, return y coordinate of furthest-down spot
+   *    (return null if filled)
+   */
 
-function checkForWin() {
+  findSpotInCol(x) {
+    for (let y = this.height - 1; y >= 0; y--) {
+      if (this.board[y][x] === null) {
+        return y;
+      }
+    }
 
-  function _win(cells) {
+    return null;
+  }
+
+
+  /** checkForWin: check board cell-by-cell for "does a win start here?" */
+
+  checkForWin() {
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        // get "checklist" of 4 cells (starting here) for each of the different
+        // ways to win
+        const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+        const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+        const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+        const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+        // find winner (only checking each win-possibility as needed)
+        if (this._win(horiz) || this._win(vert) || this._win(diagDR) || this._win(diagDL)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  //TODO: Can this helper function be OUTSIDE of checkForWin?
+  _win(cells) {
     // Check four cells to see if they're all color of current player
     //  - cells: list of four (y, x) cells
     //  - returns true if all are legal coordinates & all match currPlayer
@@ -69,38 +89,16 @@ function checkForWin() {
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
-        y < HEIGHT &&
+        y < this.height && //TODO: do we lose reference of this here?
         x >= 0 &&
-        x < WIDTH &&
-        gameState.board[y][x] === gameState.currPlayer
+        x < this.width &&
+        this.board[y][x] === this.currPlayer
     );
   }
 
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      // get "checklist" of 4 cells (starting here) for each of the different
-      // ways to win
-      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
-      // find winner (only checking each win-possibility as needed)
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-        return true;
-      }
-    }
-  }
-  return false;
 }
 
 
 export {
-  WIDTH,
-  HEIGHT,
-  gameState,
-  makeBoard,
-  findSpotInCol,
-  checkForWin,
-  switchCurrPlayer,
+  Game
 };
